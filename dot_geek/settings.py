6fe92ac24cf51bb429ecb56c219e66bbe09040f9
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-10-30 15:32:17 +0000

    Added urls for pages app and sert up static files

--- dot_geek/settings.py
+++ dot_geek/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dot_geek.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dot_geek/static')
+]
