9ca57711e813971a47bb8167a2d9d7d497d1a283
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-11-11 12:25:13 +0000

    Styled top navigation bar

--- dot_geek/settings.py
+++ dot_geek/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
