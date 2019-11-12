8adfee7c23b5637a694b558fd140c32f47e4ee30
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-11-12 13:44:47 +0000

    Setup registration views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Test error')
+        # Get values from registration form
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        uname = request.POST['uname']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check passwords
+        if password == password2:
+            # Check username & email
+            if User.objects.filter(username=uname).exists():
+                messages.error(request, 'That username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is taken')
+                # If everything pass, save user in database
+                else:
+                    user = User.objects.create_user(username=uname, password=password, email=email,
+                                                    first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, ' Thank you for creating an account. You can now log in!')
+                    return redirect('login')
+            return redirect('index')
+        else:
+            messages.error(request, 'Passwords do not match')
         return redirect('register')
     return render(request, 'accounts/register.html')
 
--- dot_geek/settings.py
+++ dot_geek/settings.py
@@ -140,4 +140,5 @@ MEDIA_URL = '/media/'
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
 }
