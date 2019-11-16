57461a64687ed686eed8cb585bc952b442af29b8
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-11-16 16:20:25 +0000

    Carousel loaded dynamicly from jinja template

--- pages/models.py
+++ pages/models.py
@@ -4,5 +4,6 @@ class IndexCarousel(models.Model):
     image = models.ImageField(upload_to='photos/carousel')
     heading = models.CharField(max_length=150, blank=True)
     paragraph = models.CharField(max_length=300, blank=True)
+    first = models.BooleanField(default=False)
     def __str__(self):
         return self.heading
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from .models import IndexCarousel
+
+carousels = IndexCarousel.objects.all()
+
+context = {
+    'carousels': carousels
+}
+
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context)
