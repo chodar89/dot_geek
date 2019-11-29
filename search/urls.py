e17eaa9dee9ae3830300f69dac947e61096918e8
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-11-29 18:30:51 +0000

    Linked dropdown navar links to new views and templates

--- search/urls.py
+++ search/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.search, name='search'),
+    path('navbar_brand/<int:brand_id>', views.navbar_brand, name='navbar_brand'),
+    path('navbar_type/<int:type_id>', views.navbar_type, name='navbar_type'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 
 from products.models import ProductBrand, ProductType, Product, SizeChart
@@ -14,7 +14,7 @@ def search(request):
     # Keywords from search form
     if 'keywords' in request.GET:
         keywords = request.GET['keywords']
-        # If its not empty string
+        # If it's not empty string
         if keywords:
             search_products = search_products.filter(Q(description__icontains=keywords)|
                                                      Q(series__icontains=keywords)|
@@ -30,17 +30,80 @@ def search(request):
     # Category from select search form
     if 'type' in request.GET:
         get_type = request.GET['type']
+        # If it's not empty
         if get_type:
             search_products = search_products.filter(product_type=get_type)
+
+    # Check prce range from search form
+    if 'price-range' in request.GET:
+        price_range = request.GET['price-range']
+        # If it's not empty
+        if price_range == "0-10":
+            search_products = search_products.filter(price__gte=0, price__lte=10)
+        if price_range == "10-20":
+            search_products = search_products.filter(price__gte=10, price__lte=20)
+        if price_range == "20-30":
+            search_products = search_products.filter(price__gte=20, price__lte=30)
+        if price_range == "30-40":
+            search_products = search_products.filter(price__gte=30, price__lte=40)
+        if price_range == "40-50":
+            search_products = search_products.filter(price__gte=40, price__lte=50)
+        if price_range == "50+":
+            search_products = search_products.filter(price__gte=50)
 
     get_brands = ProductBrand.objects.all()
 
     get_types = ProductType.objects.all()
 
+    size_chart = SizeChart.objects.all()
+
     context = {
         "get_brands": get_brands,
         "get_types": get_types,
         "search_products": search_products,
+        "size_chart": size_chart,
     }
 
     return render(request, 'search/search.html', context)
+
+def navbar_brand(request, brand_id):
+    """ Get all products with brand id from dropdown navbar """
+
+    # Get all filtered products ordered by(newest first)
+    search_products = Product.objects.all().filter(is_for_sale=True).filter(brand=brand_id).order_by('-created_at')
+
+    get_brands = ProductBrand.objects.all()
+
+    get_types = ProductType.objects.all()
+
+    size_chart = SizeChart.objects.all()
+
+    context = {
+        "get_brands": get_brands,
+        "get_types": get_types,
+        "search_products": search_products,
+        "size_chart": size_chart,
+    }
+
+    return render(request, 'search/navbar_brand.html', context)
+
+def navbar_type(request, type_id):
+    """ Get all products with category id from dropdown navbar """
+
+     # Get all filtered products ordered by(newest first)
+    search_products = Product.objects.all().filter(is_for_sale=True).filter(product_type=type_id).order_by('-created_at')
+
+    get_brands = ProductBrand.objects.all()
+
+    get_types = ProductType.objects.all()
+
+    size_chart = SizeChart.objects.all()
+
+    context = {
+        "get_brands": get_brands,
+        "get_types": get_types,
+        "search_products": search_products,
+        "size_chart": size_chart,
+    }
+
+    return render(request, 'search/navbar_type.html', context)
