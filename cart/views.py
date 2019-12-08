c5f421a8c9f64d83a25b3b6688eff04b020d3ae4
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-12-08 19:43:01 +0000

    Cart button and context_processors - Cart button on navbar - Cart button shows how many items is in cart if none its grey - move cart_details view to context_processors - move context_processors.py file from pages app to products app

--- cart/views.py
+++ cart/views.py
@@ -69,21 +69,8 @@ def add_to_cart(request, product_id):
         cart_item.save()
     return redirect('cart_details')
 
-def cart_details(request, total=0, counter=0, cart_items=None):
+def cart_details(request):
     """
-    Check all items in cart, count qnty and total price and render cart page
+    View that render cart page
     """
-    user_id = _get_user_or_none(request)
-    session_cart_id = _cart_id(request)
-    try:
-        if user_id:
-            cart = Cart.objects.get(user=user_id)
-        else:
-            cart = Cart.objects.get(cart_id=session_cart_id)
-        cart_items = CartItem.objects.filter(cart=cart, active=True)
-        for item in cart_items:
-            total += (item.product.price * item.quantity)
-            counter += item.quantity
-    except ObjectDoesNotExist:
-        pass
-    return render(request, 'cart/cart.html', dict(cart_items=cart_items, total=total, counter=counter))
+    return render(request, 'cart/cart.html')
--- products/context_processors.py
+++ products/context_processors.py
@@ -1,8 +1,27 @@
 from django.core.exceptions import ObjectDoesNotExist
 
+from products.models import ProductBrand, ProductType
+
 from cart.models import Cart, CartItem
 from cart.views import _cart_id, _get_user_or_none
 
+
+def get_brands_types_for_navbar(request):
+
+    user_id = _get_user_or_none(request)
+    session_cart_id = _cart_id(request)
+    cart_qnty = 0
+
+    nav_menu_brands = ProductBrand.objects.all().filter(is_in_navbar_menu=True)
+
+    nav_menu_category = ProductType.objects.all().filter(is_in_navbar_menu=True)
+
+    get_brands_types_for_navbar = {
+        "nav_menu_brands": nav_menu_brands,
+        "nav_menu_category": nav_menu_category,
+    }
+
+    return get_brands_types_for_navbar
 
 def cart_details(request, total=0, counter=0, cart_items=None):
     """
