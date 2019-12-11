629ee464861a7f2394cfdc9c9d532150e101712d
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-12-11 12:45:36 +0000

    Update travis file, tide up code

--- dot_geek/static/js/admin/products.js
+++ dot_geek/static/js/admin/products.js
@@ -1,12 +1,12 @@
 $(function () {
-    var selectField = $('#id_product_type'),
-        clothingStock = $('.field-stock_xs, .field-stock_s, .field-stock_m, .field-stock_l, .field-stock_xl, .field-stock_xxl, .field-stock_xxxl');
-    stock = $('.field-stock');
+    let selectField = $('#id_product_type');
+    let clothingStock = $('.field-stock_xs, .field-stock_s, .field-stock_m, .field-stock_l, .field-stock_xl, .field-stock_xxl, .field-stock_xxxl');
+    let stock = $('.field-stock');
 
     function toggleVerified(value) {
         if (value == 1) {
             clothingStock.show();
-            stock.hide();
+            stock.show();
         } else {
             clothingStock.hide();
             stock.show();
--- products/context_processors.py
+++ products/context_processors.py
@@ -22,23 +22,3 @@ def get_brands_types_for_navbar(request):
     }
 
     return get_brands_types_for_navbar
-
-def cart_details(request, total=0, counter=0, cart_items=None):
-    """
-    Check all items in cart, count qnty and total price and
-    return dict as a context processors so it can be displayed in nav bar
-    """
-    user_id = _get_user_or_none(request)
-    session_cart_id = _cart_id(request)
-    try:
-        if user_id:
-            cart = Cart.objects.get(user=user_id)
-        else:
-            cart = Cart.objects.get(cart_id=session_cart_id)
-        cart_items = CartItem.objects.filter(cart=cart, active=True)
-        for item in cart_items:
-            total += (item.product.price * item.quantity)
-            counter += item.quantity
-    except ObjectDoesNotExist:
-        pass
-    return dict(cart_items=cart_items, total=total,counter=counter)
