2caaa3eb34887d4fa665d0015405059e42b9f706
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2020-01-07 15:08:10 +0000

    Fix bug. Now when user login items append to cart that was hold in DB instead of loosing it

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -13,6 +13,17 @@ def item_counter(request):
         user_id = _get_user_or_none(request)
         if user_id:
             cart = Cart.objects.filter(user=user_id)[:1]
+            try:
+                # Get all items from session cart and assign
+                # to user cart when user logged in.
+                cart_session = Cart.objects.get(cart_id=_cart_id(request))
+                cart_session_items = CartItem.objects.all().filter(cart=cart_session)
+                get_cart_id = cart.get()
+                for item in cart_session_items:
+                    item.cart = get_cart_id
+                    item.save()
+            except Cart.DoesNotExist:
+                pass
         else:
             try:
                 cart = Cart.objects.get(cart_id=_cart_id(request))
