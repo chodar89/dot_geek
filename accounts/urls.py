f3a1096efb8228dbaab20d8f58c95aa229fd0d6c
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2020-01-16 13:24:04 +0000

    Add password reset feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
+    path('reset', include('django.contrib.auth.urls')),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
