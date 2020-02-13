a750e42df14aff5fa38778a55f474f26a560a4df
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2020-02-13 14:49:55 +0000

    Typo in utility

--- search/utility.py
+++ search/utility.py
@@ -5,7 +5,7 @@ Utility functions for search views
 def _get_price(price_range):
     if price_range == "0-10":
         gte = 0
-        lte = 20
+        lte = 10
     if price_range == "10-20":
         gte = 10
         lte = 20
